9ca2c985d4da578ca95ba3ef2ea48c44c18c0b17
Administrator <Administrator@DESKTOP-DQ5SUSC>
2020-07-18 16:50:12 +0900

    dd

---
+++ src/db/test.java
@@ -0,0 +1,18 @@
+package db;
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		int c = b;
+		b = a;
+		a = c;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
